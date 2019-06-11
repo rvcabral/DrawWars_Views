e6021053a25cb777d30d0c8433e1d21391898f94
briceno39318 <39318@alunos.isel.ipl.pt>
2019-06-11 19:23:34 +0100

    added middle phase after scores

--- draw-wars/src/components/wars/Scores.vue
+++ draw-wars/src/components/wars/Scores.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="scores">
-        <h1 class="heading-primary gradient-heading">War Scores</h1>
-        <ul>
+        <h1 class="heading-primary gradient-heading">{{scoresHeading}}</h1>
+        <ul v-if="scoresPhase">
             <li v-for="(score, index) in $root.scores" :key="score.username + index">
                 {{score.username}} - {{score.score}}
             </li>
@@ -10,24 +10,48 @@
 </template>
 
 <script>
+import { setTimeout } from 'timers';
+
 export default {
     data() {
         return {
-            scores: []
+            scores: [],
+            scoresPhase: true
         }
     },
     created() {
         this.$root.showCounter = true;
         this.$eventBus.$on('timer-finished', this.timerFinished);
+        this.$eventBus.$on('NextRound', this.nextRound);
     },
     methods: {
         timerFinished() {
-            console.log("Invoked: ResultsShown: ");
-            this.$root.connection.invoke('ResultsShown', this.$root.sessionId);
+            if(this.scoresPhase){
+                console.log("Invoked: ResultsShown: ");
+                this.$root.connection.invoke('ResultsShown', this.$root.sessionId);
+            }else {
+                console.log("Invoked: RoundEndedAck: ");
+                this.$root.connection.invoke('RoundEndedAck', this.$root.sessionId);
+            }
+        },
+        nextRound(res){
+            console.log("received: NextRound");
+            this.$eventBus.$emit('clear-timer');
+            this.$root.timeout = res;
+            this.scoresPhase = false;
+            setTimeout(() => {
+                this.$root.showCounter = true;
+            }, 500);
+        }
+    },
+    computed:{
+        scoresHeading(){
+            return this.scoresPhase ? 'War Scores' : 'Get ready for next round!';
         }
     },
     beforeDestroy(){
         this.$eventBus.$off('timer-finished', this.timerFinished);
+        this.$eventBus.$off('NextRound', this.timerFinished);
     }
 }
 </script>
